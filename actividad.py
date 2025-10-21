7b889ba820577b80f5c7b17e023f5a90844fd5e8
Scarpone25 <ciganocap@gmail.com>
2025-10-21 13:48:01 -0500

    Create actividad.py

---
+++ actividad.py
@@ -0,0 +1,11 @@
+import time
+import sys
+
+def actividad(intervalo=600):
+    while True:
+        print("Manteniendo actividad en el server xd")  # Imprime algo en la terminal
+        sys.stdout.flush()  # Asegura que el mensaje se imprima de inmediato en la terminal
+        time.sleep(interval)  # Espera el intervalo especificado (en segundos)
+
+if __name__ == "__main__":
+    actividad(intervalo=600)
